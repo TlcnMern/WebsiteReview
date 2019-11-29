8a949c0c2839cd514e683c7b225b8792be629734
huyhoang <huyhoangpro1001@gmail.com>
2019-11-30 02:01:06 +0700

    update

--- client/src/components/admin/analytics/GoogleChart.js
+++ client/src/components/admin/analytics/GoogleChart.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import 'font-awesome/css/font-awesome.min.css';
+// import "../../../public/stylesheets/partials/style.css"
+// import "../../../public/stylesheets/partials/styleAdmin.css"
 import { Chart } from "react-google-charts";
 class GoogleChart extends Component {
     render() {
-        console.log(this.props.data)
         return (
             <div className="row mt-5">  
                 <div className="col-md-12">
@@ -14,7 +17,21 @@ class GoogleChart extends Component {
                                 width={'100%'}
                                 chartType="Bar"
                                 loader={<div>Loading First Chart</div>}
-                                data={this.props.data}
+                                data={[
+                                    ['Month', 'Film', 'Food','Book','Travel'],
+                                    ['Tháng 1', 1000, 400, 200,390],
+                                    ['Tháng 2', 1170, 460, 250,599],
+                                    ['Tháng 3', 660, 1120, 300,786],
+                                    ['Tháng 4', 1030, 540, 350,222],
+                                    ['Tháng 5', 1030, 540, 350,999],
+                                    ['Tháng 6', 930, 490, 260,1000],
+                                    ['Tháng 7', 1000, 400, 200,390],
+                                    ['Tháng 8', 1170, 460, 250,599],
+                                    ['Tháng 9', 660, 1120, 300,786],
+                                    ['Tháng 10', 1030, 540, 350,222],
+                                    ['Tháng 11', 1030, 540, 350,999],
+                                    ['Tháng 12', 930, 490, 260,1000],
+                                ]}
                                 options={{
                                     // Material design options
                                     chart: {
