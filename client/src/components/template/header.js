45b738f111c3d32061af78643641a6d430f8a3bc
Vu <nguyentuanvu231198@gmail.com>
2019-11-29 13:20:10 +0700

    update

--- client/src/components/template/header.js
+++ client/src/components/template/header.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'font-awesome/css/font-awesome.min.css';
 import "../../public/stylesheets/partials/style.css"
+import "../../public/stylesheets/partials/styleResponesive.css"
 import logo from '../../public/images/logo1.png';
 import BoxSearch from '../search/BoxSearch';
 
 class HeaderTemplate extends Component {
 
-  render() {
-    return (
-      <div className="topnav">
-        <div className="row">
-          <div className="col-sm-2">
-            <Link to="/">
-              <img src={logo} style={{marginLeft: '20px',maxWidth: '230px',maxHeight: '40px',width: 'auto',height: 'auto'}} alt="2R4U" />
-            </Link>
-          </div>
-          <div className="col-sm-7">
-            <span className="txtTOP">TRANG CHỦ</span>
-          </div>
-          <BoxSearch/>
-        </div>
+    render() {
+        return ( <
+            div className = "topnav" >
+            <
+            div className = "row" >
+            <
+            div className = "col-sm-2" >
+            <
+            Link to = "/" >
+            <
+            img src = { logo }
+            style = {
+                { marginLeft: '20px', maxWidth: '230px', maxHeight: '40px', width: 'auto', height: 'auto' } }
+            alt = "2R4U" / >
+            <
+            /Link>
 
-      </div>
-    );
-  }
+            <
+            /div> <
+            div className = "col-sm-7" >
+            <
+            span className = "txtTOP" > TRANG CHỦ < /span> <
+            /div> <
+            BoxSearch / >
+            <
+            /div>
+
+            <
+            /div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+    auth: state.auth
 });
 
 
